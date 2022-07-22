76252da594c2a4e3ad1ad24867b265fe16187846
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-07-22 19:20:41 +0200

    Python
    Included the argparse package which allowed me
    to take input from the CLI when the script is
    called. For now it takes the name of the .txt
    file and displays it's content to the console.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import argparse as arg
+
 def main():
-    print('Hello world!')
+    parser=arg.ArgumentParser(description='Cyrillic to latin text file converter')
+    parser.add_argument('filename',
+                        help='Name of the file',
+                        type=str)
+    
+    fileName=parser.parse_args()
+    fileName=fileName.filename
+    
+    file_handler=open(fileName)
+    for line in file_handler:
+        print(line.rstrip())
 
 if __name__ == '__main__':
     main()
