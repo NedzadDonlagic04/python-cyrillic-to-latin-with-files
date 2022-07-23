5b111bc7f97d9e89767ab5925921f9a09a93fa00
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-07-23 18:06:57 +0200

    Python
    Finished the entire project.
    Imported all the modules needed for the script to
    work.The script checks did the optional flag get
    raised at the start,if it has it goes into the if
    condition which checks does the file exists and is
    it in the right format (.txt), after which it does
    the conversion,if the optional flag didn't get
    raised the script goes into the else condition
    which gives the user an input prompt where they
    can enter the cyrillic they wish to convert.

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
+import os
 import argparse as arg
+import cyrtranslit as cyr
 
 def main():
     parser=arg.ArgumentParser(description='Cyrillic to latin text file converter')
-    parser.add_argument('filename',
+    parser.add_argument('-f','--filename',
                         help='Name of the file',
                         type=str)
     
     fileName=parser.parse_args()
-    fileName=fileName.filename
     
-    file_handler=open(fileName)
-    for line in file_handler:
-        print(line.rstrip())
+    fileName=fileName.filename
+
+    if fileName:
+        if not os.path.exists(fileName):
+            print("File doesn't exist!")
+            quit()
+
+        if not fileName.endswith('.txt'):
+            print("File has to be a text file!")
+            quit()
+        
+        print('The conversion from the file is:')
+        with open(fileName) as file:
+            for line in file:
+                line=cyr.to_latin(line)
+                print(line.rstrip())
+    else:
+        inputText=input('Enter the cyrillic to convert: ')
+        converted=cyr.to_latin(inputText)
+        print(f'The converted text is: {converted}')
 
 if __name__ == '__main__':
     main()
